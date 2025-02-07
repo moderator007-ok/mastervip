9d0836780c851b8eacd166010ea21c9d40503610
moderator007-ok <meizharryputtar@gmail.com>
2025-02-07 21:12:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.INFO,    
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt='%d-%b-%y %H:%M:%S'
 )
@@ -23,23 +23,16 @@ if __name__ == "__main__":
         plugins=plugins,
         workers=10,
     )
-
     async def main():
         await bot.start()
         bot_info = await bot.get_me()
         LOGGER.info(f"<--- @{bot_info.username} Started --->")
-
         for user_id in Config.AUTH_USERS:
             try:
-                await bot.send_message(
-                    chat_id=user_id, 
-                    text=f"__Congrats! You Are DRM member ... if You get any error then contact me -  {Config.CREDIT}__"
-                )
+                await bot.send_message(chat_id=user_id, text=f"__Congrats! You Are DRM member ... if You get any error then contact me -  {Config.CREDIT}__ ")
             except Exception as e:
                 LOGGER.error(f"Failed to send message to user {user_id}: {e}")
                 continue
-
         await idle()
-
-    asyncio.run(main())
-    LOGGER.info("<---🦄 OK 😌💞 --->")
+    asyncio.get_event_loop().run_until_complete(main())
+    LOGGER.info("<---🦅 Stopped baby💞 --->")
